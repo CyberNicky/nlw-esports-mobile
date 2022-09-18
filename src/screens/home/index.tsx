f6cc2780e3bc7c9eb16d2de73d68edf46927856e
Monique Campos <nickyalbuquerque2008@gmail.com>
2022-09-18 17:31:16 -0300

    feat: integration with backend

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,12 +2,21 @@
 import { View, Image, FlatList } from 'react-native';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../componets/Heading';
-import { GameCard } from '../../componets/GameCard';
+import { GameCard, GameCardProps } from '../../componets/GameCard';
 import { GAMES } from '../../utils/games';
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from '@react-native-safe-area-context';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+    useEffect(() =>{
+      fetch('http://192.168.43.8:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+    }, []);
   return (
     <View style={styles.container}>
       <Image 
@@ -20,7 +29,7 @@ export function Home() {
       subtitle='Selecione o game que deseja jogar...'
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({item})=> (
       
